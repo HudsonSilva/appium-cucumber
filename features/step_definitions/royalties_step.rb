f9f84021e338ee9767a5c9bfcb4d9714723912ba
Hudson Muniz <hudson.muniz@concrete.com.br>
2019-01-07 16:45:02 -0200

    steps definitions royalties

---
+++ features/step_definitions/royalties_step.rb
@@ -0,0 +1,15 @@
+Dado("que meu nome já foi informado") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("busco por um vendedor") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("escolho o ano") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("vejo o total de vendas no ano escolhido") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
